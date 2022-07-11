eb213e9108458f10ad0ea4d52f63f3105dc55232
Alex <alex.y.kuznetsov@gmail.com>
2022-07-11 17:43:03 +0500

    conflict

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,12 +1,9 @@
 function sum(a, b) {
-  try {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-      throw new TypeError('Both arguments must be numbers');
-    }
-    return a + b;
-  } finally {
-    console.log('done');
+  if ([a, b].some((value) => typeof value !== 'number')) {
+    throw new TypeError();
   }
+
+  return a + b;
 }
 
 module.exports = sum;
