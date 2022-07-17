6b2be85390b508c119f95a26594c49d148d2149b
Alex <alex.y.kuznetsov@gmail.com>
2022-07-17 17:33:45 +0500

    module-3 task-5

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,4 +1,4 @@
-const url = require('url');
+const fs = require('fs');
 const http = require('http');
 const path = require('path');
 
@@ -13,12 +13,42 @@ server.on('request', (req, res) => {
   switch (req.method) {
     case 'DELETE':
 
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Path not found');
+        break;
+      }
+
+      const stream = fs.createReadStream(filepath);
+      stream.pipe(res);
+
+      stream.on('error', (error) => {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File not found');
+        } else {
+          res.statusCode = 500;
+          res.end('Something went wrong');
+        }
+      });
+
+      stream.on('end', () => {
+        fs.unlink(filepath, () => {
+          res.statusCode = 200;
+          res.end('File deleted');
+        });
+      });
+
       break;
 
     default:
       res.statusCode = 501;
       res.end('Not implemented');
   }
+
+  req.on('aborted', () => {
+    stream.destroy();
+  });
 });
 
 module.exports = server;
