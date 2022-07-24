933425170232cf5906f50c8b36fa2d4406880b0d
Alex <alex.y.kuznetsov@gmail.com>
2022-07-24 12:36:26 +0500

    module-3 task-2

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,26 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.string = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const stringFromChunk = chunk.toString();
+    this.string += stringFromChunk;
+    const split = this.string.split(os.EOL);
+    this.string = split.pop();
+
+    for (let i = 0; i < split.length; i++) {
+      this.push(split[i]);
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.string);
+    callback();
   }
 }
 
