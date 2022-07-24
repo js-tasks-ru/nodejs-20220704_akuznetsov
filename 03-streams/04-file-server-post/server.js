e84a50293c3b9aa21f0f5b7a9887ac2881ef98c1
Alex <alex.y.kuznetsov@gmail.com>
2022-07-24 14:07:50 +0500

    module-2 task-4

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,7 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -12,6 +13,49 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Path not found');
+        break;
+      }
+
+      if (fs.existsSync(filepath)) {
+        res.statusCode = 409;
+        res.end('Path already exists');
+        break;
+      }
+
+      const limit = new LimitSizeStream({limit: 1048576});
+      const stream = fs.createWriteStream(filepath);
+      req.pipe(limit).pipe(stream);
+
+      limit.on('error', (error) => {
+        if (error.code === 'LIMIT_EXCEEDED') {
+          fs.unlink(filepath, () => {
+            res.statusCode = 413;
+            res.end('Exceeded file size limit');
+          });
+          stream.destroy();
+        }
+      });
+
+      stream
+          .on('error', () => {
+            res.statusCode = 500;
+            res.end('Something went wrong');
+          })
+          .on('finish', () => {
+            res.statusCode = 201;
+            res.end('Successfully created file');
+          });
+
+      req.on('aborted', () => {
+        fs.unlink(filepath, () => {
+          res.end('Connection lost, file deleted');
+        });
+        stream.destroy();
+      });
 
       break;
 
